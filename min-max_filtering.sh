5f68724c8375e900d17038f81697911cf9ff8b5f
Bella Sinclair <bsm.sinclair@gmail.com>
2019-06-19 14:19:01 +0200

    set parameters for vcftools

--- min-max_filtering.sh
+++ min-max_filtering.sh
@@ -7,4 +7,12 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bsm.sinclair@gmail.com
 
-module load bioinfo-tools Vcftools/
+module load bioinfo-tools vcftools/0.1.15
+
+# --minDP sets the minimum depth
+# --maxDP sets the maximum depth
+# --missing-indv gives the number of how missingness there are on the individual level (missing SNPs/indv)
+# --missing-site the missing genotype/site
+
+vcftools --vcf ../0_data/parva.vcf --minDP 5 --maxDP 100 --missing-indv --missing-site --recode --redoce-INFO-all --out parva_filtered_depth
+
