7e302fd4bc2aaa44923c1c8f5fcf1be1886f640d
Bella Sinclair <bsm.sinclair@gmail.com>
2019-06-20 09:56:48 +0200

    changed maxDP to 95 and added filtering for taiga

--- 1_filtering/min-max_filtering.sh
+++ 1_filtering/min-max_filtering.sh
@@ -2,7 +2,7 @@
 #SBATCH -A p2018002
 #SBATCH -p core
 #SBATCH -n 1
-#SBATCH -t 00:30:00
+#SBATCH -t 00:50:00
 #SBATCH -J min-max_filtering
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bsm.sinclair@gmail.com
@@ -11,8 +11,8 @@ module load bioinfo-tools vcftools/0.1.15
 
 # --minDP sets the minimum depth
 # --maxDP sets the maximum depth
-# --missing-indv gives the number of how missingness there are on the individual level (missing SNPs/indv)
-# --missing-site the missing genotype/site
 
-vcftools --vcf ../0_data/parva.vcf --minDP 5 --maxDP 50 --recode --recode-INFO-all --out parva_filtered_depth
+vcftools --vcf ../0_data/parva.vcf --minDP 5 --maxDP 95 --recode --recode-INFO-all --out parva_filtered_maxdp95_mindp5
+
+vcftools -vcf ../0_data/taiga.vcf --minDP 5 --maxDP 95 --recode --recode-INFO-all --out taiga_filtered_maxdp95_mindp5 
 
