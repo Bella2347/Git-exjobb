38bd4984d4c197a088d6823267d0d3c0255c07e2
Bella Sinclair <bsm.sinclair@gmail.com>
2019-06-20 11:10:11 +0200

    changed file name for parva, ran only parva

--- 1_filtering/missing_stats.sh
+++ 1_filtering/missing_stats.sh
@@ -2,7 +2,7 @@
 #SBATCH -A p2018002
 #SBATCH -p core
 #SBATCH -n 1
-#SBATCH -t 00:30:00
+#SBATCH -t 01:00:00
 #SBATCH -J missing_stats
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bsm.sinclair@gmail.com
@@ -14,6 +14,6 @@ module load bioinfo-tools vcftools/0.1.15
 # --missing-indv gives the number of how missingness there are on the individual level (missing SNPs/indv)
 # --missing-site the missing genotype/site
 
-vcftools --vcf parva_filtered_depth.recode.vcf --missing-indv --out parva_missing_indv
-vcftools --vcf parva_filtered_depth.recode.vcf --missing-site --out parva_missing_site
+vcftools --vcf parva_filtered_maxdp95_mindp5.recode.vcf --missing-indv --out parva_maxdp95_mindp5_missing_indv
+vcftools --vcf parva_filtered_maxdp95_mindp5.recode.vcf --missing-site --out parva_maxdp95_mindp5_missing_site
 
