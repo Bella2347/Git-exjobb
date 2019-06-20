5597dbd6f379ceeb93d5ae9fe3399915df74ce38
Bella Sinclair <bsm.sinclair@gmail.com>
2019-06-20 12:34:41 +0200

    missing stats for taiga

--- 1_filtering/missing_stats.sh
+++ 1_filtering/missing_stats.sh
@@ -14,6 +14,9 @@ module load bioinfo-tools vcftools/0.1.15
 # --missing-indv gives the number of how missingness there are on the individual level (missing SNPs/indv)
 # --missing-site the missing genotype/site
 
-vcftools --vcf parva_filtered_maxdp95_mindp5.recode.vcf --missing-indv --out parva_maxdp95_mindp5_missing_indv
-vcftools --vcf parva_filtered_maxdp95_mindp5.recode.vcf --missing-site --out parva_maxdp95_mindp5_missing_site
+#vcftools --vcf parva_filtered_maxdp95_mindp5.recode.vcf --missing-indv --out parva_maxdp95_mindp5_missing_indv
+#vcftools --vcf parva_filtered_maxdp95_mindp5.recode.vcf --missing-site --out parva_maxdp95_mindp5_missing_site
+
+vcftools --vcf taiga_filtered_maxdp95_mindp5.recode.vcf --missing-indv --out taiga_maxdp95_mindp5_missing_indv
+vcftools --vcf taiga_filtered_maxdp95_mindp5.recode.vcf --missing-site --out taiga_maxdp95_mindp5_missing_site
 
