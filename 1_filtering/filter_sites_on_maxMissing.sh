a14f9d81941b9bde2d04cda14ae84e46d0bcdfe6
Bella Sinclair <bsm.sinclair@gmail.com>
2019-06-20 16:08:05 +0200

    changed limit to 0.9 instead of 0.1

--- 1_filtering/filter_sites_on_maxMissing.sh
+++ 1_filtering/filter_sites_on_maxMissing.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -l
 #SBATCH -A p2018002
 #SBATCH -p core
-#SBATCH -n 1
-#SBATCH -t 05:00:00
-#SBATCH -J DP_parva
+#SBATCH -n 2
+#SBATCH -t 04:00:00
+#SBATCH -J remove_sites
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bsm.sinclair@gmail.com
 
 module load bioinfo-tools vcftools/0.1.15
 
-vcftools --vcf ../2_vcf_with_masked_genotypes_depth/parva_filtered_maxdp95_mindp5.recode.vcf --max-missing 0.10 --out parva_filtered_maxMissing --recode --recode-INFO-all
+vcftools --vcf ../2_vcf_with_masked_genotypes_depth/parva_filtered_maxdp95_mindp5.recode.vcf --max-missing 0.90 --out parva_filtered_maxMissing_90 --recode --recode-INFO-all
 
-vcftools --vcf ../2_vcf_with_masked_genotypes_depth/taiga_filtered_maxdp95_mindp5.recode.vcf --max-missing 0.10 --out taiga_filtered_maxMissing --recode --recode-INFO-all
+vcftools --vcf ../2_vcf_with_masked_genotypes_depth/taiga_filtered_maxdp95_mindp5.recode.vcf --max-missing 0.90 --out taiga_filtered_maxMissing_90 --recode --recode-INFO-all
 
