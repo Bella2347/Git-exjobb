02e22662dfc10ba49dda0fb0a2fb9c7dfc522cc3
Bella Sinclair <bsm.sinclair@gmail.com>
2019-06-20 14:01:01 +0200

    removed line to look at INFO dp

--- 1_filtering/geno_depth.sh
+++ 1_filtering/geno_depth.sh
@@ -3,10 +3,11 @@
 #SBATCH -p core
 #SBATCH -n 1
 #SBATCH -t 01:00:00
-#SBATCH -J geno_depth_parva
+#SBATCH -J DP_parva
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bsm.sinclair@gmail.com
 
 module load bioinfo-tools vcftools/0.1.15
 
 vcftools --vcf ../parva_filtered_maxdp95_mindp5.recode.vcf --out parva_maxdp95_mindp5 --geno-depth
+
