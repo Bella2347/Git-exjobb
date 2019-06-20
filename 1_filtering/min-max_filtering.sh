a07193932877aa79db9f7cc1a59bda9692ec1476
Bella Sinclair <bsm.sinclair@gmail.com>
2019-06-20 11:09:48 +0200

    fixed spelling, ran taiga alone

--- 1_filtering/min-max_filtering.sh
+++ 1_filtering/min-max_filtering.sh
@@ -2,7 +2,7 @@
 #SBATCH -A p2018002
 #SBATCH -p core
 #SBATCH -n 1
-#SBATCH -t 00:50:00
+#SBATCH -t 03:00:00
 #SBATCH -J min-max_filtering
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bsm.sinclair@gmail.com
@@ -12,7 +12,7 @@ module load bioinfo-tools vcftools/0.1.15
 # --minDP sets the minimum depth
 # --maxDP sets the maximum depth
 
-vcftools --vcf ../0_data/parva.vcf --minDP 5 --maxDP 95 --recode --recode-INFO-all --out parva_filtered_maxdp95_mindp5
+#vcftools --vcf ../0_data/parva.vcf --minDP 5 --maxDP 95 --recode --recode-INFO-all --out parva_filtered_maxdp95_mindp5
 
-vcftools -vcf ../0_data/taiga.vcf --minDP 5 --maxDP 95 --recode --recode-INFO-all --out taiga_filtered_maxdp95_mindp5 
+vcftools --vcf ../0_data/taiga.vcf --minDP 5 --maxDP 95 --recode --recode-INFO-all --out taiga_filtered_maxdp95_mindp5 
 
