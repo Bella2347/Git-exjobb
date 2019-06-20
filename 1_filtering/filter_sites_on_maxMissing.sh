2b7e34629db4870400d5851cf91222d1a3b05a5a
Bella Sinclair <bsm.sinclair@gmail.com>
2019-06-20 14:18:45 +0200

    changed path to files

--- 1_filtering/filter_sites_on_maxMissing.sh
+++ 1_filtering/filter_sites_on_maxMissing.sh
@@ -9,7 +9,7 @@
 
 module load bioinfo-tools vcftools/0.1.15
 
-vcftools --vcf parva_filtered_maxdp95_mindp5.recode.vcf --max-missing 0.10 --out parva_filtered_maxMissing
+vcftools --vcf ../2_vcf_with_masked_genotypes_depth/parva_filtered_maxdp95_mindp5.recode.vcf --max-missing 0.10 --out parva_filtered_maxMissing
 
-vcftools --vcf taiga_filtered_maxdp95_mindp5.recode.vcf --max-missing 0.10 --out taiga_filtered_maxMissing
+vcftools --vcf ../2_vcf_with_masked_genotypes_depth/taiga_filtered_maxdp95_mindp5.recode.vcf --max-missing 0.10 --out taiga_filtered_maxMissing
 
