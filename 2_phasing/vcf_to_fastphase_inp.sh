b47b36c80efcbd9e018e86cbe3acc57d524cf9f5
Bella Sinclair <bsm.sinclair@gmail.com>
2019-06-24 15:56:26 +0200

    tried plink on different file

--- 2_phasing/vcf_to_fastphase_inp.sh
+++ 2_phasing/vcf_to_fastphase_inp.sh
@@ -2,12 +2,12 @@
 #SBATCH -A p2018002
 #SBATCH -p core
 #SBATCH -n 1
-#SBATCH -t 10:00:00
+#SBATCH -t 01:00:00
 #SBATCH -J vcf_plink_fastphase
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bsm.sinclair@gmail.com
 
 module load bioinfo-tools plink/1.90b4.9
 
-plink --vcf ../../1_filtering/1_genotype_depth/parva_filtered_n00001.vcf --out parva_plink_n00001 --recode fastphase-1chr --allow-extra-chr
+plink --vcf chrFal34.vcf --out parva_plink_chrFal34 --recode fastphase --allow-extra-chr
 
