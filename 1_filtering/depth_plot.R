5c2a8498fab19e98b3e8272c4709862c2f309442
Bella Sinclair <bsm.sinclair@gmail.com>
2019-06-24 13:45:45 +0200

    added dim to create matrix

--- 1_filtering/depth_plot.R
+++ 1_filtering/depth_plot.R
@@ -1,17 +1,17 @@
 library(vcfR)
 
-parva_n00001 <- read.vcfR("parva_subset_n00001.vcf")
+parva_n00001 <- read.vcfR("parva_filtered_n00001.vcf")
 
 parva_n00001_dp <- extract.gt(parva_n00001, element="DP", as.numeric=TRUE)
 parva_n00001_dp_df <- as.data.frame(parva_n00001_dp)
-c(n,m) <- dim(parva_n00001_dp_df)
+n <- dim(parva_n00001_dp_df)
 
 
 parva_n00001_gt <- extract.gt(parva_n00001, element="GT", as.numeric=FALSE)
 parva_n00001_gt[is.na(parva_n00001_gt)] <- FALSE
 parva_n00001_gt[parva_n00001_gt != FALSE] <- TRUE
 parva_n00001_logi <- as.logical(parva_n00001_gt)
-parva_n00001_logi <- data.frame(matrix(parva_n00001_logi, nrow=n, ncol=m))
+parva_n00001_logi <- data.frame(matrix(parva_n00001_logi, nrow=n[1], ncol=n[2]))
 colnames(parva_n00001_logi) <- colnames(parva_n00001_dp_df)
 row.names(parva_n00001_logi) <- row.names(parva_n00001_dp_df)
 
