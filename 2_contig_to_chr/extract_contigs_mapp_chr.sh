d21165f166e325bc87c84e90f798a6684bbaab01
Bella Sinclair <bsm.sinclair@gmail.com>
2019-06-24 16:28:23 +0200

    extract all contigs mapping to chrs

--- 2_contig_to_chr/extract_contigs_mapp_chr.sh
+++ 2_contig_to_chr/extract_contigs_mapp_chr.sh
@@ -8,15 +8,14 @@
 #SBATCH --mail-user bsm.sinclair@gmail.com
 
 # Adds the first info lines
-cat ../1_vcf_head/1_info_lines.txt > chrFal34.vcf
-#parva_chrContigs.vcf
+cat ../1_vcf_head/1_info_lines.txt > parva_chrContigs.vcf
 
-# Adds the contigs present in this files
-while read LINE; do grep "$LINE" ../1_vcf_head/2_contigs.txt >> chrFal34.vcf; done < ../2_chr_contigs/ChrFal34.txt 
+# Adding contigs present in this files
+while read LINE; do grep "$LINE" ../1_vcf_head/2_contigs.txt >> parva_chrContigs.vcf; done < ../2_chr_contigs/contigs.txt 
 
 # Adds the head that explains each column
-cat ../1_vcf_head/3_head.txt >> chrFal34.vcf
-#parva_chrContigs.vcf
+cat ../1_vcf_head/3_head.txt >> parva_chrContigs.vcf
 
 # Adds all the SNPs in the contigs specified
-while read LINE; do grep "^$LINE" ../../../1_filtering/3_removed_sites_with_missing-genotype/parva_filtered_maxMissing_90.recode.vcf >> chrFal34.vcf; done < ../2_chr_contigs/ChrFal34.txt
+while read LINE; do grep "^$LINE" ../../../1_filtering/3_removed_sites_with_missing-genotype/parva_filtered_maxMissing_90.recode.vcf >> parva_chrContigs.vcf; done < ../2_chr_contigs/contigs.txt
+
