32d21a54508d1042fe2ff610a3156aced96b1d00
Bella Sinclair <bsm.sinclair@gmail.com>
2019-06-25 12:52:19 +0200

    added more hours

--- 2_contig_to_chr/extract_contigs_mapp_chr.sh
+++ 2_contig_to_chr/extract_contigs_mapp_chr.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 #SBATCH -A p2018002
 #SBATCH -p core
-#SBATCH -n 1
-#SBATCH -t 01:00:00
+#SBATCH -n 2
+#SBATCH -t 05:00:00
 #SBATCH -J chr_contigs_to_vcf
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bsm.sinclair@gmail.com
