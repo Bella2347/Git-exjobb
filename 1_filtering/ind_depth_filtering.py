4472f764637b10ccc5141533f808469ec8dfc61a
Bella Sinclair <bsm.sinclair@gmail.com>
2019-06-26 09:12:32 +0200

    added comments and elapsed time

--- 1_filtering/ind_depth_filtering.py
+++ 1_filtering/ind_depth_filtering.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import time
 from operator import eq
 
 if not len(sys.argv)==4:
@@ -14,11 +15,13 @@ if sys.argv[1]==sys.argv[3]:
 # Read in vcf file and depth file
 vcf = open(sys.argv[1], 'r')
 max_depths = open(sys.argv[2], 'r')
+# Open the outfile for writing
 out = open(sys.argv[3], 'w+')
 
 
+# Initiate a list with the filtering depths
 max_depth_list = []
-# split the depths into a list
+# Save the filtering depths from the depth-file to the list
 for line in max_depths:
 	if not line.startswith('#'):
 		max_depth_list.append(line.strip('\n'))
@@ -28,31 +31,37 @@ max_depths.close()
 print("Max depths to be used:")
 print(max_depth_list)
 
+
+start = time.time()
+
 print("Masking sites...")
+# Step through each line in the vcf...
 for line in vcf:
-	# If it starts with info lines, keep those
+	# print the lines that starts with # directoly to the out-file
 	if line.startswith('#'):
 		out.write(line)
 	else:
-		# Split into columns
+		# Split the variant lines into columns
 		columns = line.split('\t')
+		# In the 10th column the info for each sample start, check that the number of samples in the vcf and depth-file are the same
 		if not len(columns)-9 == len(max_depth_list):
 			print("Error:\tNumber of samples in VCF does not match number of samples in depth file\n")
 			sys.exit()
-
+		# Initiate a list for the depth of each sample for a variant
 		depths = []
+		# Step through all sample columns and extract the DP (3rd field)
 		for i in range(9,len(columns)):
 			depths.append(columns[i].split(':')[2])
-		# Generate list with true and false if samples dp is larger than filter dp for that
-		logi_list = []
+		# If the depth for that variant is larger than the value for that sample in the depth-file the genotype is masked
 		for i in range(len(max_depth_list)):
-			logi_list.append(depths[i] > max_depth_list[i])
-		for i in range(len(logi_list)):
-			if logi_list[i]:
+			if depths[i] > max_depth_list[i]:
 				columns[i+9] = re.sub('\d/\d:','./.:', columns[i+9])
+		# Join all columns again, tab-separated
 		masked_line = '\t'.join(columns)
+		# Write the filtered line to the out-file
 		out.write(masked_line)
 
 vcf.close()
+elapsed = time.time() - start
 
-print("Done!")
+print("Done! Ran in: %.2fs" % elapsed )
