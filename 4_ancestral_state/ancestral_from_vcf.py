d8535d875c83ee98ea5eab48a7f2cfbc310ee85e
Bella Sinclair <bsm.sinclair@gmail.com>
2019-06-27 11:03:47 +0200

    extracts the state as expected

--- 4_ancestral_state/ancestral_from_vcf.py
+++ 4_ancestral_state/ancestral_from_vcf.py
@@ -1,60 +1,94 @@
+# Scrip to extract the ancestral state from a vcf file containing individuals from three groups
+# Input is a vcf-file and a group-file which specify the number of individuals in each group, the individuals are assumed to appear in group order
+
 import sys
 import re
 
+# Open the input files
 vcf =  open(sys.argv[1], 'r')
-ingroup_file =  open(sys.argv[2], 'r')
-outgroup1_file =  open(sys.argv[3], 'r')
-outgroup2_file =  open(sys.argv[4], 'r')
+group_file =  open(sys.argv[2], 'r')
+
+# Save the number of individuals for each group in a list
+group_len = [int(line.strip('\n')) for line in group_file]
+
+group_file.close()
 
 
-ingroup = [line.strip('\n') for line in ingroup_file]
-outgroup1 = [line.strip('\n') for line in outgroup1_file]
-outgroup2 = [line.strip('\n') for line in outgroup2_file]
+for line in vcf:
+	if not line.startswith('#'):
+		# Split in to columns
+		columns = line.strip('\n').split('\t')
 
-ingroup_file.close()
-outgroup1_file.close()
-outgroup2_file.close()
+		# Save the ref and alt alleles
+		alleles = []
+		alleles.append(columns[3])
+		alt_alleles = columns[4].split(',')
+		for alt in alt_alleles:
+			alleles.append(alt)
 
-print(ingroup, outgroup1, outgroup2)
 
-#for line in vcf:
-#	if not line.startswith('#'):
-#		columns = line.strip('\n').split('\t')
-#		alleles = columns[2] #columns with the alleles
-#
-#		ingroup_genotypes = []
-#		outgroup1_genotypes = []
-#		outgroup2_genotypes = []
-#
-#		for i in range(9,end_of_ingroup):
-#			genotype = re.find('\d/\d', columns[i])
-#			if genotype != './.':
-#				ingroup_genotypes.append(genotype)
-#
-#		for i in range(end_of_ingroup+1,end_of_outgroup1):
-#			genotype = re.find('\d/\d', columns[i])
-#			if genotype != './.':
-#			outgroup1_genotypes.append(genotype)
-#
-#		for i in range(end_of_outgroup1,end_of_outgroup2):
-#			genotype = re.find('\d/\d', columns[i])
-#			if genotype != './.':
-#			outgroup2_genotypes.append(genotype)
-#		
-#		# If any are heterozygous it is false
-#		monomorphic = []
-#		if not any(element != ingroup_genotypes[0] for element in ingroup_genotypes):
-#			monomorphic.append(ingroup_genotype[0])
-#		
-#		if not any(element != outgroup1_genotypes[0] for element in outgroup1_genotypes):
-#			monomorphic.append(outgroup1_genotype[0])
-#
-#		if not any(element != outgroup2_genotypes[0] for element in outgroup2_genotypes):
-#			monomorphic.append(outgroup2_genotype[0])
+		ingroup_genotypes = []
+		outgroup1_genotypes = []
+		outgroup2_genotypes = []
 
-		# compare if all elements in monomorphic are the same, at least two
+		in_start = 9
+		out1_start = group_len[0]+9
+		out2_start = group_len[0]+int(group_len[1])+9
+		
+		# Go through all indv in a group and save the genotype
+		for i in range(in_start, out1_start):
+			genotype = re.findall('\d/\d', columns[i])
+			if len(genotype) != 0:
+				genotype = genotype[0].split('/')
+				for alt in genotype:
+					ingroup_genotypes.append(alt)
 
-			# Print allele at position monomorphic
+		for i in range(out1_start, out2_start):
+			genotype = re.findall('\d/\d', columns[i])
+			if len(genotype) != 0:
+				genotype = genotype[0].split('/')
+				for alt in genotype:
+					outgroup1_genotypes.append(alt)
 
+		for i in range(out2_start, len(columns)):
+			genotype = re.findall('\d/\d', columns[i])
+			if len(genotype) != 0:
+				genotype = genotype[0].split('/')
+				for alt in genotype:
+					outgroup2_genotypes.append(alt)
+
+		# Check all genotypes in each group, if all are the same, save the genotype
+		monomorphic = []
+		if not any(element != ingroup_genotypes[0] for element in ingroup_genotypes):
+			monomorphic.append(ingroup_genotypes[0])
+		
+		if not any(element != outgroup1_genotypes[0] for element in outgroup1_genotypes):
+			monomorphic.append(outgroup1_genotypes[0])
+
+		if not any(element != outgroup2_genotypes[0] for element in outgroup2_genotypes):
+			monomorphic.append(outgroup2_genotypes[0])
+
+		# If at least two groups are monomorphic check if at least two are monomorphic for the same variant
+		# If they are monomorphic for the same, print that allele, if not print a ?
+		if len(monomorphic) == 2:
+			if monomorphic[0] == monomorphic[1]:
+				state = alleles[int(monomorphic[0])]
+			else:
+				state = '?'
+		elif len(monomorphic) == 3:
+			if monomorphic[0] == monomorphic[1]:
+				state = alleles[int(monomorphic[0])]
+			elif monomorphic[0] == monomorphic[2]:
+				state = alleles[int(monomorphic[0])]
+			elif monomorphic[1] == monomorphic[2]:
+				state = alleles[int(monomorphic[1])]
+			else:
+				state = '?'
+		else:
+			state = '?'
+		
+		# Print the state and the chr and position
+		out = columns[0] + '\t' + columns[1] + '\t'
+		print(out + state)
 
 
