e5d190b4b1fa9af08a5bbccb2a345c56c5d5990e
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-01 13:55:26 +0200

    tried to phase .inp with * removed

--- 3_phasing/phase.sh
+++ 3_phasing/phase.sh
@@ -2,12 +2,12 @@
 #SBATCH -A p2018002
 #SBATCH -p core
 #SBATCH -n 1
-#SBATCH -t 05:00:00
+#SBATCH -t 00:30:00
 #SBATCH -J phasing
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bsm.sinclair@gmail.com
 
 module load bioinfo-tools fastPHASE/1.4.8
 
-fastPHASE -oparva_n00001_phased -C50 -KL5 -KU15 -KI5 ../2_input_format/1_chrFal34/parva_plink_chrFal34..chr-N00705.recode.phase.inp
+fastPHASE -oparva_n00001_phased -C50 -KL5 -KU15 -KI5 ../2_input_format/parva_n00001.no_indels.inp
 
