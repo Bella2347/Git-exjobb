6a868aa3336f92276bf19f9fdef0826770bd2947
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-01 13:54:59 +0200

    filter using --max_allele 2

--- 1_filtering/filter_indels.sh
+++ 1_filtering/filter_indels.sh
@@ -2,12 +2,12 @@
 #SBATCH -A p2018002
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 02:00:00
+#SBATCH -t 00:10:00
 #SBATCH -J filter_indels
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bsm.sinclair@gmail.com
 
 module load bioinfo-tools vcftools/0.1.15
 
-vcftools --vcf parva_n00001_filtered_maxMissing_90.recode.vcf --remove-indels --out parva_n00001_maxdepth_maxmissing_indels --recode --recode-INFO-all
+vcftools --vcf parva_n00001_filtered_maxMissing_90.recode.vcf --max-alleles 2 --out parva_n00001_maxdepth_maxmissing_biallelic --recode --recode-INFO-all
 
