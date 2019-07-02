bd621f5a1f9e931229fa7195a5a051bdce9fb10d
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-02 13:16:36 +0200

    filter parva_chrContigs.vcf

--- 1_filtering/filter_indels.sh
+++ 1_filtering/filter_indels.sh
@@ -2,12 +2,12 @@
 #SBATCH -A p2018002
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 00:10:00
+#SBATCH -t 01:00:00
 #SBATCH -J filter_indels
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bsm.sinclair@gmail.com
 
 module load bioinfo-tools vcftools/0.1.15
 
-vcftools --vcf ../2_missing_remove_sites/parva_n00001_maxDepth_maxMissingSite.recode.vcf --max-alleles 2 --out parva_n00001_maxDepth_maxMissingSite_keepBiallelic --recode --recode-INFO-all
+vcftools --vcf ../../0_data/subset_vcf/parva_chrContigs.vcf --max-alleles 2 --out parva_keepBiallelic --recode --recode-INFO-all
 
