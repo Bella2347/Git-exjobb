4125bb9293f810a886582b8022cbf406747d7ee6
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-02 13:48:43 +0200

    run on parva_chrContigs

--- 1_filtering/filter_sites_on_maxMissing.sh
+++ 1_filtering/filter_sites_on_maxMissing.sh
@@ -9,5 +9,5 @@
 
 module load bioinfo-tools vcftools/0.1.15
 
-vcftools --vcf parva_n00001_maxDepth.vcf --max-missing 0.90 --out parva_maxDepth_maxMissingSite --recode --recode-INFO-all
+vcftools --vcf ../parva_chrContigs_keepBiallelic_maxDepth.vcf --max-missing 0.90 --out parva_keepBiallelic_maxDepth_maxMissingSite --recode --recode-INFO-all
 
