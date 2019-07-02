a44c8386367cace507e734bb9a6102784f824095
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-02 13:54:20 +0200

    changed path

--- 1_filtering/filter_sites_on_maxMissing.sh
+++ 1_filtering/filter_sites_on_maxMissing.sh
@@ -2,12 +2,12 @@
 #SBATCH -A p2018002
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 00:30:00
+#SBATCH -t 04:00:00
 #SBATCH -J remove_sites
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bsm.sinclair@gmail.com
 
 module load bioinfo-tools vcftools/0.1.15
 
-vcftools --vcf ../parva_chrContigs_keepBiallelic_maxDepth.vcf --max-missing 0.90 --out parva_keepBiallelic_maxDepth_maxMissingSite --recode --recode-INFO-all
+vcftools --vcf ../1_depth_filter/parva_chrContigs_keepBiallelic_maxDepth.vcf --max-missing 0.90 --out parva_keepBiallelic_maxDepth_maxMissingSite --recode --recode-INFO-all
 
