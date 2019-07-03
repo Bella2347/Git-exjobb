51ac842b21b70320eefdf914960a007d2bfd9e24
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-03 13:38:31 +0200

    converts fastPHASE out to LDhelmet in

---
+++ 4_LD_estimation/ldhelmet_inp.py
@@ -0,0 +1,41 @@
+import sys
+import re
+import time
+
+if not len(sys.argv)==3:
+        print('\nError:\tincorrect number of command-line arguments')
+        print('Syntax:\tldhelmet_inp.py [FastPHASE Output] [Output File]\n')
+        sys.exit()
+
+if sys.argv[1]==sys.argv[2]:
+        print('\nError:\tinput-file and output-file are the same, choose another output-file\n')
+        sys.exit()
+
+fastphase_file = open(sys.argv[1], 'r')
+out_file = open(sys.argv[2], 'w+')
+
+print('Input: ' + sys.argv[1] + '\nOutput: ' + sys.argv[2])
+
+print_line = 0
+
+t_start = time.time()
+
+print('Converting fastPHASE output to LDhelmet input...')
+
+for line in fastphase_file:
+	if line.startswith('# ID'):
+		sample_id = re.findall('#\sID\s(\d+)', line)
+		out_file.write('>sample_' + sample_id[0] + '\n')
+		print_line = 1
+	elif print_line == 1:
+		bases = line.strip('\n').split(' ')
+		sequence = ''.join(bases)
+		out_file.write(sequence + '\n')
+
+fastphase_file.close()
+out_file.close()
+
+elapsed = time.time() - t_start
+
+print('Done! Ran in: %.2fs' % elapsed)
+
