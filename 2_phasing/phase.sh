29e9674482a7dfcabef8544878c8b4758403caec
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-03 08:53:28 +0200

    phasing n00012

--- 2_phasing/phase.sh
+++ 2_phasing/phase.sh
@@ -2,12 +2,12 @@
 #SBATCH -A p2018002
 #SBATCH -p core
 #SBATCH -n 1
-#SBATCH -t 00:30:00
+#SBATCH -t 05:00:00
 #SBATCH -J phasing
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bsm.sinclair@gmail.com
 
 module load bioinfo-tools fastPHASE/1.4.8
 
-fastPHASE -oparva_n00001_phased -C50 -KL5 -KU15 -KI5 2_input_format/parva_n00001.inp
+fastPHASE -oparva_N00012_phased -C50 -KL5 -KU15 -KI5 1_input_format/parva.chr-N00012.recode.phase.inp
 
