b32f8edee49ba0abe357e01a7e1b3fdf04b3ab3f
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-03 15:26:20 +0200

    parameters for ldhelmet

--- 4_LD_estimation/ldhelmet.sh
+++ 4_LD_estimation/ldhelmet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH -A p2018002
 #SBATCH -p core
-#SBATCH -n 1
+#SBATCH -n 5
 #SBATCH -t 05:00:00
 #SBATCH -J ldhelmet
 #SBATCH --mail-type=ALL
@@ -9,5 +9,7 @@
 
 module load bioinfo-tools LDhelmet/1.10
 
-
+ldhelmet rjmcmc -o parva_N00042 --pos_file 1_input_format/pos_files/parva_N00042.pos \
+--snps_file 1_input_format/seq_files/parva_N00042_LDhelmetInp.seq \
+-n 2000000 --burn_in 200000 -b 10 -m 1_input_format/mut_mat.txt --num_threds 5 -w 50
 
