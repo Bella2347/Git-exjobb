c310d1c96fe0431b3fbde80750ef7af0715eed01
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-10 10:58:34 +0200

    fixed path error

--- 2_phasing/phase.sh
+++ 2_phasing/phase.sh
@@ -9,13 +9,13 @@
 
 module load bioinfo-tools fastPHASE/1.4.0
 
-fastPHASE -oparva_N00001_phased -C25 -H-1 -KL3 -KU28 -KI5 parva_N00001_noP.inp
+fastPHASE -oparva_N00001_phased -C25 -H-1 -KL3 -KU28 -KI5 ../parva_N00001_noP.inp
 
-fastPHASE -oparva_N00025_phased -C25 -H-1 -KL3 -KU28 -KI5 ../1_input_format/parva.chr-N00025.recode.phase.inp
+#fastPHASE -oparva_N00025_phased -C25 -H-1 -KL3 -KU28 -KI5 ../1_input_format/parva.chr-N00025.recode.phase.inp
 
-fastPHASE -oparva_N00215_phased -C25 -H-1 -KL3 -KU28 -KI5 ../1_input_format/parva.chr-N00215.recode.phase.inp
+#fastPHASE -oparva_N00215_phased -C25 -H-1 -KL3 -KU28 -KI5 ../1_input_format/parva.chr-N00215.recode.phase.inp
 
-fastPHASE -oparva_N00323_phased -C25 -H-1 -KL3 -KU28 -KI5 ../1_input_format/parva.chr-N0323.recode.phase.inp
+fastPHASE -oparva_N00323_phased -C25 -H-1 -KL3 -KU28 -KI5 ../1_input_format/parva.chr-N00323.recode.phase.inp
 
 
 
