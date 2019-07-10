459f46fcf2e35a0423908bfc3ab7e7659dce606b
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-10 10:30:37 +0200

    run phasing on four files of different sizes

--- 2_phasing/phase.sh
+++ 2_phasing/phase.sh
@@ -1,13 +1,21 @@
 #!/bin/bash -l
 #SBATCH -A p2018002
 #SBATCH -p core
-#SBATCH -n 3
-#SBATCH -t 05:00:00
+#SBATCH -n 5
+#SBATCH -t 10:00:00
 #SBATCH -J phasing
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bsm.sinclair@gmail.com
 
 module load bioinfo-tools fastPHASE/1.4.0
 
-fastPHASE -oparva_N00049_phased_140 -C50 -KL5 -KU25 -KI5 ../1_input_format/parva.chr-N00049.recode.phase.inp
+fastPHASE -oparva_N00001_phased -C25 -H-1 -KL3 -KU28 -KI5 parva_N00001_noP.inp
+
+fastPHASE -oparva_N00025_phased -C25 -H-1 -KL3 -KU28 -KI5 ../1_input_format/parva.chr-N00025.recode.phase.inp
+
+fastPHASE -oparva_N00215_phased -C25 -H-1 -KL3 -KU28 -KI5 ../1_input_format/parva.chr-N00215.recode.phase.inp
+
+fastPHASE -oparva_N00323_phased -C25 -H-1 -KL3 -KU28 -KI5 ../1_input_format/parva.chr-N0323.recode.phase.inp
+
+
 
