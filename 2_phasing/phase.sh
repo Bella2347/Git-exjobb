6eb4ef5f9b1bf1a39c7791b527512624cb15ff69
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-10 12:55:06 +0200

    added N00100 and the flag -g to not out genotypes

--- 2_phasing/phase.sh
+++ 2_phasing/phase.sh
@@ -2,20 +2,20 @@
 #SBATCH -A p2018002
 #SBATCH -p core
 #SBATCH -n 5
-#SBATCH -t 10:00:00
+#SBATCH -t 2:00:00
 #SBATCH -J phasing
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bsm.sinclair@gmail.com
 
 module load bioinfo-tools fastPHASE/1.4.0
 
-fastPHASE -oparva_N00001_phased -C25 -H-1 -KL3 -KU28 -KI5 ../parva_N00001_noP.inp
+# fastPHASE -oparva_N00001_phased -C25 -H-1 -KL3 -KU28 -KI5 ../parva_N00001_noP.inp
 
 #fastPHASE -oparva_N00025_phased -C25 -H-1 -KL3 -KU28 -KI5 ../1_input_format/parva.chr-N00025.recode.phase.inp
 
 #fastPHASE -oparva_N00215_phased -C25 -H-1 -KL3 -KU28 -KI5 ../1_input_format/parva.chr-N00215.recode.phase.inp
 
-fastPHASE -oparva_N00323_phased -C25 -H-1 -KL3 -KU28 -KI5 ../1_input_format/parva.chr-N00323.recode.phase.inp
+fastPHASE -oparva_N00323_phased -C25 -H-1 -g -KL3 -KU28 -KI5 ../1_input_format/parva.chr-N00323.recode.phase.inp
 
-
+fastPHASE -oparva_N00100_phased -C25 -H-1 -g -KL3 -KU28 -KI5 ../1_input_format/parva.chr-N00100.recode.phase.inp
 
