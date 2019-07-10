a708223c56dced2f1a0bc102126ffad16ffa3308
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-10 13:23:34 +0200

    run with subpopulation information

--- 2_phasing/phase.sh
+++ 2_phasing/phase.sh
@@ -9,13 +9,13 @@
 
 module load bioinfo-tools fastPHASE/1.4.0
 
-# fastPHASE -oparva_N00001_phased -C25 -H-1 -KL3 -KU28 -KI5 ../parva_N00001_noP.inp
+fastPHASE -oparva_N00001_phased_wSub -C25 -H-1 -g -KL3 -KU28 -KI5 -u../../0_data/parva_subpopulations.txt ../parva_N00001_noP.inp
 
-#fastPHASE -oparva_N00025_phased -C25 -H-1 -KL3 -KU28 -KI5 ../1_input_format/parva.chr-N00025.recode.phase.inp
+fastPHASE -oparva_N00025_phased_wSub -C25 -H-1 -g -KL3 -KU28 -KI5 -u../../0_data/parva_subpopulations.txt ../1_input_format/parva.chr-N00025.recode.phase.inp
 
-#fastPHASE -oparva_N00215_phased -C25 -H-1 -KL3 -KU28 -KI5 ../1_input_format/parva.chr-N00215.recode.phase.inp
+# fastPHASE -oparva_N00215_phased -C25 -H-1 -g -KL3 -KU28 -KI5 ../1_input_format/parva.chr-N00215.recode.phase.inp
 
-fastPHASE -oparva_N00323_phased -C25 -H-1 -g -KL3 -KU28 -KI5 ../1_input_format/parva.chr-N00323.recode.phase.inp
+# fastPHASE -oparva_N00323_phased -C25 -H-1 -g -KL3 -KU28 -KI5 ../1_input_format/parva.chr-N00323.recode.phase.inp
 
-fastPHASE -oparva_N00100_phased -C25 -H-1 -g -KL3 -KU28 -KI5 ../1_input_format/parva.chr-N00100.recode.phase.inp
+# fastPHASE -oparva_N00100_phased -C25 -H-1 -g -KL3 -KU28 -KI5 ../1_input_format/parva.chr-N00100.recode.phase.inp
 
