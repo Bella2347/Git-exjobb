d0978e39cbe5a0b40e02491be2bed5008abb0ac1
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-12 08:46:29 +0200

    changed path

--- 2_phasing/phase.sh
+++ 2_phasing/phase.sh
@@ -11,13 +11,13 @@ module load bioinfo-tools fastPHASE/1.4.0
 
 #fastPHASE -oparva_N00602_1phased -T50 -C30 -K6 -u../../0_data/parva_subpopulations.txt ../1_input_format/parva.chr-N00602.noP.inp
 
-fastPHASE -oparva_N00400_1phased -T50 -C30 -K6 -u../../0_data/parva_subpopulations.txt ../1_input_format/parva.chr-N00400.noP.inp
+fastPHASE -oparva_N00400_1phased -T50 -C30 -K6 -u../0_data/parva_subpopulations.txt 1_input_format/parva.chr-N00400.noP.inp
 
-fastPHASE -oparva_N00300_1phased -T50 -C30 -K6 -u../../0_data/parva_subpopulations.txt ../1_input_format/parva.chr-N00300.noP.inp
+fastPHASE -oparva_N00300_1phased -T50 -C30 -K6 -u../0_data/parva_subpopulations.txt 1_input_format/parva.chr-N00300.noP.inp
 
 #fastPHASE -oparva_N00200_1phased -T50 -C30 -K6 -u../../0_data/parva_subpopulations.txt ../1_input_format/parva.chr-N00200.noP.inp
 
-fastPHASE -oparva_N00070_1phased -T50 -C30 -K6 -u../../0_data/parva_subpopulations.txt ../1_input_format/parva.chr-N00070.noP.inp
+fastPHASE -oparva_N00070_1phased -T50 -C30 -K6 -u../0_data/parva_subpopulations.txt 1_input_format/parva.chr-N00070.noP.inp
 
 #fastPHASE -oparva_N00020_1phased -T50 -C30 -K6 -u../../0_data/parva_subpopulations.txt ../1_input_format/parva.chr-N00020.noP.inp
 
