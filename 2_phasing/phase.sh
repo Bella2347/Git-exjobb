a14ef62e8871c3ebb012d00c68dd5af4351a487c
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-12 08:44:56 +0200

    run with -K6 -T50 -C30 for N00400, N00200, N00070

--- 2_phasing/phase.sh
+++ 2_phasing/phase.sh
@@ -9,18 +9,18 @@
 
 module load bioinfo-tools fastPHASE/1.4.0
 
-fastPHASE -oparva_N00602_1phased -T20 -C25 -H-1 -KL2 -KU20 -KI2 -u../../0_data/parva_subpopulations.txt ../1_input_format/parva.chr-N00602.noP.inp
+#fastPHASE -oparva_N00602_1phased -T50 -C30 -K6 -u../../0_data/parva_subpopulations.txt ../1_input_format/parva.chr-N00602.noP.inp
 
-fastPHASE -oparva_N00400_1phased -T20 -C25 -H-1 -KL2 -KU20 -KI2 -u../../0_data/parva_subpopulations.txt ../1_input_format/parva.chr-N00400.noP.inp
+fastPHASE -oparva_N00400_1phased -T50 -C30 -K6 -u../../0_data/parva_subpopulations.txt ../1_input_format/parva.chr-N00400.noP.inp
 
-fastPHASE -oparva_N00300_1phased -T20 -C25 -H-1 -KL2 -KU20 -KI2 -u../../0_data/parva_subpopulations.txt ../1_input_format/parva.chr-N00300.noP.inp
+fastPHASE -oparva_N00300_1phased -T50 -C30 -K6 -u../../0_data/parva_subpopulations.txt ../1_input_format/parva.chr-N00300.noP.inp
 
-fastPHASE -oparva_N00200_1phased -T20 -C25 -H-1 -KL2 -KU20 -KI2 -u../../0_data/parva_subpopulations.txt ../1_input_format/parva.chr-N00200.noP.inp
+#fastPHASE -oparva_N00200_1phased -T50 -C30 -K6 -u../../0_data/parva_subpopulations.txt ../1_input_format/parva.chr-N00200.noP.inp
 
-fastPHASE -oparva_N00070_1phased -T20 -C25 -H-1 -KL2 -KU20 -KI2 -u../../0_data/parva_subpopulations.txt ../1_input_format/parva.chr-N00070.noP.inp
+fastPHASE -oparva_N00070_1phased -T50 -C30 -K6 -u../../0_data/parva_subpopulations.txt ../1_input_format/parva.chr-N00070.noP.inp
 
-fastPHASE -oparva_N00020_1phased -T20 -C25 -H-1 -KL2 -KU20 -KI2 -u../../0_data/parva_subpopulations.txt ../1_input_format/parva.chr-N00020.noP.inp
+#fastPHASE -oparva_N00020_1phased -T50 -C30 -K6 -u../../0_data/parva_subpopulations.txt ../1_input_format/parva.chr-N00020.noP.inp
 
-fastPHASE -oparva_N00001_1phased -T20 -C25 -H-1 -KL2 -KU20 -KI2 -u../../0_data/parva_subpopulations.txt ../1_input_format/parva.chr-N00001.noP.inp
+# fastPHASE -oparva_N00001_1phased -T50 -C30 -K6 -u../../0_data/parva_subpopulations.txt ../1_input_format/parva.chr-N00001.noP.inp
 
 
