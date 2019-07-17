b5476f6fb5cc059d53997c28d9d958551b4a7674
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-17 11:09:42 +0200

    try making conf table from seq files

--- 4_LD_estimation/ldhelmet.sh
+++ 4_LD_estimation/ldhelmet.sh
@@ -2,14 +2,17 @@
 #SBATCH -A p2018002
 #SBATCH -p core
 #SBATCH -n 5
-#SBATCH -t 05:00:00
+#SBATCH -t 10:00:00
 #SBATCH -J ldhelmet
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bsm.sinclair@gmail.com
 
 module load bioinfo-tools LDhelmet/1.10
 
-ldhelmet rjmcmc -o parva_N00042 --pos_file 1_input_format/pos_files/parva_N00042.pos \
---snps_file 1_input_format/seq_files/parva_N00042_LDhelmetInp.seq \
--n 2000000 --burn_in 200000 -b 10 -m 1_input_format/mut_mat.txt --num_threads 5 -w 50
+ldhelmet find_confs --num_threads 5 -w 50 -o parva_N70_N400.conf ../2_phasing/parva_N00070_1ldhelmet.inp.seq ../2_phasing/parva_N00400_ldhelmet.inp.seq
+
+
+#ldhelmet rjmcmc -o parva_N00042 --pos_file 1_input_format/pos_files/parva_N00042.pos \
+#--snps_file 1_input_format/seq_files/parva_N00042_LDhelmetInp.seq \
+#-n 2000000 --burn_in 200000 -b 10 -m 1_input_format/mut_mat.txt --num_threads 5 -w 50
 
