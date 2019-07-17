eaf3a6584fd318eb6759c5b9a512cedcea0c7dab
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-17 11:11:18 +0200

    added space to commit rm

--- 4_LD_estimation/ldhelmet.sh
+++ 4_LD_estimation/ldhelmet.sh
@@ -12,7 +12,7 @@ module load bioinfo-tools LDhelmet/1.10
 ldhelmet find_confs --num_threads 5 -w 50 -o parva_N70_N400.conf ../2_phasing/parva_N00070_1ldhelmet.inp.seq ../2_phasing/parva_N00400_ldhelmet.inp.seq
 
 
-#ldhelmet rjmcmc -o parva_N00042 --pos_file 1_input_format/pos_files/parva_N00042.pos \
+# ldhelmet rjmcmc -o parva_N00042 --pos_file 1_input_format/pos_files/parva_N00042.pos \
 #--snps_file 1_input_format/seq_files/parva_N00042_LDhelmetInp.seq \
 #-n 2000000 --burn_in 200000 -b 10 -m 1_input_format/mut_mat.txt --num_threads 5 -w 50
 
