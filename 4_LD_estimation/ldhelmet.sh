203a7039fd36887d2174fdae5f8a4b9e60b073ca
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-18 13:12:44 +0200

    run only rjmcmc, not post to text

--- 4_LD_estimation/ldhelmet.sh
+++ 4_LD_estimation/ldhelmet.sh
@@ -19,7 +19,7 @@ module load bioinfo-tools LDhelmet/1.10
 ldhelmet rjmcmc --num_threads 5 -w 50 -l 1_input_format/parva_N70_N400.lk -b 10 --snps_file 1_input_format/seq_files/parva_N00400_ldhelmet.inp.seq --pos_file 1_input_format/pos_files/parva.chr-N00400.pos -m 1_input_format/mut_mat.txt --burn_in 100000 -n 1000000 -o parva_N00400.post
 
 # Extract info
-ldhelmet post_to_text -m -p 0.025 -p 0.50 -p 0.0975 -o parva_N00400_out.txt parva_N00400.post
+# ldhelmet post_to_text -m -p 0.025 -p 0.50 -p 0.0975 -o parva_N00400_out.txt parva_N00400.post
 
 
 
