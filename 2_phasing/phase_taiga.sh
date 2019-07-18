f1ec6516793c38c15b0002feea13d9e5c0302464
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-18 10:48:25 +0200

    it timed out, run just N33

--- 2_phasing/phase_taiga.sh
+++ 2_phasing/phase_taiga.sh
@@ -10,9 +10,9 @@
 module load bioinfo-tools fastPHASE/1.4.0
 
 # Select K
-fastPHASE -otaiga_N00270_phased_T20_C25 -T20 -C25 -KL2 -KU26 -KI2 -u../0_data/taiga_subpopulations.txt 1_input_format/taiga.chr-N00270.noP.inp
+#fastPHASE -otaiga_N00270_phased_T20_C25 -T20 -C25 -KL2 -KU26 -KI2 -u../0_data/taiga_subpopulations.txt 1_input_format/taiga.chr-N00270.noP.inp
 
-fastPHASE -otaiga_N00139_phased_T20_C25 -T20 -C25 -KL2 -KU26 -KI2 -u../0_data/taiga_subpopulations.txt 1_input_format/taiga.chr-N00139.noP.inp
+#fastPHASE -otaiga_N00139_phased_T20_C25 -T20 -C25 -KL2 -KU26 -KI2 -u../0_data/taiga_subpopulations.txt 1_input_format/taiga.chr-N00139.noP.inp
 
 fastPHASE -otaiga_N00033_phased_T20_C25 -T20 -C25 -KL2 -KU26 -KI2 -u../0_data/taiga_subpopulations.txt 1_input_format/taiga.chr-N00033.noP.inp
 
