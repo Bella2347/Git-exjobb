fd8276bd28bb513c9a5caf97634083feef635517
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-23 14:55:29 +0200

    makes the positions indexed from zero

--- 2_phasing/remove_P.py
+++ 2_phasing/remove_P.py
@@ -1,3 +1,6 @@
+# Script to remove the position row in the fastphase.inp files and saves the positions in 
+# a separate file indexed from 0
+
 import sys
 import re
 
@@ -11,8 +14,10 @@ pos_out = open(out_name[0]+'.pos', 'w+')
 
 for line in in_file:
 	if line.startswith('P'):
-		pos = line.strip('\n').split(' ')
+		pos = line.strip('\n').strip(' ').split(' ')
 		pos = pos[1:]	# Remove the P in the begining
+		for i in range(0,len(pos)):
+			pos[i] = str(int(pos[i])-1) # Subtracts 1 to make the index from 0
 		pos = '\n'.join(pos)
 		pos_out.write(pos)
 	else:
