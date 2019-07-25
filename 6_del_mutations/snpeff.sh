81558e9a5205a654ffb13f49d7e218e496d9b07d
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-25 13:52:29 +0200

    run snpeff

---
+++ 6_del_mutations/snpeff.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#SBATCH -A p2018002
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 05:00:00
+#SBATCH -J snpEff
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user bsm.sinclair@gmail.com
+
+module load bioinfo-tools snpEff/4.3t
+
+snpEff FicAlb_1.4.86 ../1_scaff_names/parva_chrContigs_keepBiallelic_maxDepth_maxMissingSite.FicAlb1.4.vcf > parva_chrContigs_snpeff.vcf
+
+
+
