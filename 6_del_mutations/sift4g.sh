3097ab10ef08a664004deffcc0f4a61149150619
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-25 17:00:09 +0200

    added path to all directories

--- 6_del_mutations/sift4g.sh
+++ 6_del_mutations/sift4g.sh
@@ -7,5 +7,5 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bsm.sinclair@gmail.com
 
-java -jar /home/bella/SIFT4G_Annotator_v2.4.jar -c -t -i 1_scaff_names/parva_chrContigs_keepBiallelic_maxDepth_maxMissingSite.NCBIv1.vcf -d FicAlb_1.4.83 -r 
+java -jar /home/bella/SIFT4G_Annotator_v2.4.jar -c -t -i 1_scaff_names/parva_chrContigs_keepBiallelic_maxDepth_maxMissingSite.NCBIv1.vcf -d /home/bella/FicAlb_1.4.83/ -r /proj/sllstore2017033/nobackup/work/bella/6_del_mutations/
 
