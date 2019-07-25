5a211fa2a97cebef42c5ebc24967a0ca78bb452b
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-25 16:00:06 +0200

    trying to run sift

---
+++ 6_del_mutations/sift4g.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+#SBATCH -A p2018002
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 05:00:00
+#SBATCH -J sift4G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user bsm.sinclair@gmail.com
+
+java -jar /home/bella/SIFT4G_Annotator_v2.4.jar -c -t -i 1_scaff_names/parva_chrContigs_keepBiallelic_maxDepth_maxMissingSite.NCBIv1.vcf -d FicAlb_1.4.83 -r 
+
