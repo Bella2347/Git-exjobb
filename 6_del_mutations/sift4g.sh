276032156473354715f2a5f449f6a64c4fdfc0c1
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-26 09:43:08 +0200

    does not work, changed to run from home directory

--- 6_del_mutations/sift4g.sh
+++ 6_del_mutations/sift4g.sh
@@ -2,10 +2,10 @@
 #SBATCH -A p2018002
 #SBATCH -p core
 #SBATCH -n 1
-#SBATCH -t 05:00:00
+#SBATCH -t 02:00:00
 #SBATCH -J sift4G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bsm.sinclair@gmail.com
 
-java -jar /home/bella/SIFT4G_Annotator_v2.4.jar -c -t -i 1_scaff_names/parva_chrContigs_keepBiallelic_maxDepth_maxMissingSite.NCBIv1.vcf -d /home/bella/FicAlb_1.4.83/ -r /proj/sllstore2017033/nobackup/work/bella/6_del_mutations/
+java -jar SIFT4G_Annotator_v2.4.jar -c -t -i /proj/sllstore2017033/nobackup/work/bella/6_del_mutations/1_scaff_names/parva_chrContigs_keepBiallelic_maxDepth_maxMissingSite.NCBIv1.vcf -d FicAlb_1.4.83/ -r /proj/sllstore2017033/nobackup/work/bella/6_del_mutations/
 
