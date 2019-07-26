75376eb81715aabd401d5cf0cc3a671a859029be
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-26 10:46:53 +0200

    changed paths

--- 6_del_mutations/sift4g.sh
+++ 6_del_mutations/sift4g.sh
@@ -7,5 +7,5 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bsm.sinclair@gmail.com
 
-java -jar SIFT4G_Annotator_v2.4.jar -c -t -i /proj/sllstore2017033/nobackup/work/bella/6_del_mutations/1_scaff_names/parva_chrContigs_keepBiallelic_maxDepth_maxMissingSite.NCBIv1.vcf -d FicAlb_1.4.83/ -r /proj/sllstore2017033/nobackup/work/bella/6_del_mutations/
+java -jar /home/bella/SIFT4G_Annotator_v2.4.jar -c -t -i /proj/sllstore2017033/nobackup/work/bella/6_del_mutations/1_scaff_names/parva_sorted.NCBIv1.vcf -d /home/bella/FicAlb_1.4.83/ -r /proj/sllstore2017033/nobackup/work/bella/6_del_mutations/
 
