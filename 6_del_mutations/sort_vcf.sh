be6c7a6a8ecada1e27a9d705663aa8d0160461ae
Bella Sinclair <bsm.sinclair@gmail.com>
2019-07-26 10:03:01 +0200

    use more cores and save to vcf

--- 6_del_mutations/sort_vcf.sh
+++ 6_del_mutations/sort_vcf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH -A p2018002
 #SBATCH -p core
-#SBATCH -n 1
+#SBATCH -n 3
 #SBATCH -t 02:00:00
 #SBATCH -J sort_vcf
 #SBATCH --mail-type=ALL
@@ -9,5 +9,7 @@
 
 module load bioinfo-tools BEDTools/2.27.1
 
-bedtools sort -chrThenSizeA -i parva_chrContigs_keepBiallelic_maxDepth_maxMissingSite.NCBIv1.vcf
+grep '^#' parva_chrContigs_keepBiallelic_maxDepth_maxMissingSite.NCBIv1.vcf > parva_sorted.NCBIv1.vcf
+
+bedtools sort -chrThenSizeA -i parva_chrContigs_keepBiallelic_maxDepth_maxMissingSite.NCBIv1.vcf >> parva_sorted.NCBIv1.vcf
 
