d31725e6ca01c8d505cb5e30c394e842b7393fe4
Bella Sinclair <bsm.sinclair@gmail.com>
2019-08-01 16:07:26 +0200

    Changed the type from l to s

--- 6_analyse/plot_recomb_missense.R
+++ 6_analyse/plot_recomb_missense.R
@@ -1,9 +1,8 @@
 # Plot the recombination rate and the density of missense SNPs
 
 par(mar = c(5,5,2,5));
-plot(N70_rec[,1],N70_rec[,3], type="l", ylab="Recombination rate [1/bp]", xlim=c(0,4386342), main="Recombination rate and missense SNP density", xlab="Position [bp]");
+plot(N70_rec[,1],N70_rec[,3], type="s", ylab="Recombination rate [1/bp]", xlim=c(0,4386342), main="Recombination rate and missense SNP density", xlab="Position [bp]");
 par(new=T);
-plot(density(N70_missense), axes=F, ylab=NA, xlab=NA, main=NA, col="red");axis(side=4);mtext(side=4, line=3, "Missense SNP density");
+plot(density(N70_missense_pos), axes=F, ylab=NA, xlab=NA, main=NA, col="red");axis(side=4);mtext(side=4, line=3, "Missense SNP density");
 legend("topright", legend=c("Recombination rate", "Missense SNP density"), lty=c(1,1), col=c("black","red"))
-
 
