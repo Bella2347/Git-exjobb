5d6480507452b73d8bce9b16975d4bf2474225ce
Bella Sinclair <bsm.sinclair@gmail.com>
2019-08-13 11:00:47 +0200

    changed file names to run on allGQ30

--- chr_mapping_contigs.sh
+++ chr_mapping_contigs.sh
@@ -2,10 +2,10 @@
 #SBATCH -A p2018002
 #SBATCH -p core
 #SBATCH -n 1
-#SBATCH -t 05:00:00
-#SBATCH -J extracting_contigs_that_mapp_chr
+#SBATCH -t 02:00:00
+#SBATCH -J extractingScaffoldsThatMappToChr
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bsm.sinclair@gmail.com
 
-python3 ../../Git-exjobb/chr_mapping_contigs.py ../taiga.vcf ../contigs_lists/Chr10.txt taiga_chr10contigs.vcf
+python3 ../Git-exjobb/chr_mapping_contigs.py allGQ30.vcf contigs_lists/chr_mapping_scaffolds.txt allGQ30_chrMappingScaffolds.vcf
 
