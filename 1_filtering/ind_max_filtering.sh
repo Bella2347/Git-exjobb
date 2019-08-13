d10b6107fe62edc2cefdbeb4081a96212bfdd65b
Bella Sinclair <bsm.sinclair@gmail.com>
2019-08-13 14:09:37 +0200

    run on allGQ30

--- 1_filtering/ind_max_filtering.sh
+++ 1_filtering/ind_max_filtering.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -l
 #SBATCH -A p2018002
 #SBATCH -p core
-#SBATCH -n 2
-#SBATCH -t 04:00:00
+#SBATCH -n 10
+#SBATCH -t 10:00:00
 #SBATCH -J filter_on_individual_depth
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bsm.sinclair@gmail.com
 
-python3 ../../Git-exjobb/1_filtering/ind_depth_filtering.py ../3_indels_remove/taiga_chr10_keepBiallelic.recode.vcf taiga_depth.txt taiga_chr10_keepBiallelic_maxDepth.vcf
-
-
+python3 ../Git-exjobb/1_filtering/ind_depth_filtering.py ../0_data/allGQ30_chrMappingScaffolds.vcf allGQ30_chrMappingScaffolds_genoDepth.txt all30GQ_chrMappingScaffolds_filteredMaxDepth.vcf
