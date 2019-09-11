ad3dd80716bc1833e7862c1b8eab54e963c7f24f
Bella Sinclair <bsm.sinclair@gmail.com>
2019-09-11 12:58:51 +0200

    changed threshold

--- 2_phasing/split_lines.py
+++ 2_phasing/split_lines.py
@@ -8,14 +8,19 @@ filename = re.findall('(\w+\.chr-\w+.noP).inp', sys.argv[1])
 
 out_file = open(str(filename[0] + '.splitLines.inp'), 'w+')
 
+max_len = 900000
+
 for line in inp_file:
 	line = line.strip('\n')
 	characters = list(line)
-	if len(characters) > 500000:
-		nr_split = math.ceil(len(characters)/500000)
+	if len(characters) > max_len:
+		nr_split = math.ceil(len(characters)/max_len)
 		size_split = math.ceil(len(characters)/nr_split)
-		for i in range(0,(nr_split-2)):
-			out_file.write(''.join(characters[i*size_split:(i+1)*size_split]) + '\n')
+		if nr_split == 2:
+			out_file.write(''.join(characters[0:size_split]) + '\n')	
+		else:
+			for i in range(0,(nr_split-2)):
+				out_file.write(''.join(characters[i*size_split:(i+1)*size_split]) + '\n')
 		out_file.write(''.join(characters[(nr_split-1)*size_split:len(characters)]) + '\n')
 	else:
 		out_file.write(line + '\n')
