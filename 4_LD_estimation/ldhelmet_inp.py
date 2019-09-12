2b78add5b0e040cf452a3c31de1b2dad224302a1
Bella Sinclair <bsm.sinclair@gmail.com>
2019-09-12 10:16:51 +0200

    writes subpop. to different files

--- 4_LD_estimation/ldhelmet_inp.py
+++ 4_LD_estimation/ldhelmet_inp.py
@@ -2,49 +2,73 @@ import sys
 import re
 import time
 
-if not len(sys.argv)==3:
+
+def write_id(name, file):
+        file.write('>sample_' + name + '\n')
+
+def write_seq(line, file):
+	sequence = ''.join(line)
+	file.write(sequence + '\n')
+
+
+
+if not len(sys.argv)>=3:
         print('\nError:\tincorrect number of command-line arguments')
-        print('Syntax:\tldhelmet_inp.py [FastPHASE Output] [Output File]\n')
+        print('Syntax:\tldhelmet_inp.py [FastPHASE Output] [Output File subpop. 1] optional: [[Output File subpop. 2] [Output File subpop. 3] ...\n')
         sys.exit()
 
-if sys.argv[1]==sys.argv[2]:
-        print('\nError:\tinput-file and output-file are the same, choose another output-file\n')
-        sys.exit()
 
-fastphase_file = open(sys.argv[1], 'r')
-out_file = open(sys.argv[2], 'w+')
+out_files = [None]*(len(sys.argv)-2)
 
-print('Input: ' + sys.argv[1] + '\nOutput: ' + sys.argv[2])
+for i in range(2,len(sys.argv)):
+	out_files[i-2] = open(sys.argv[i], 'w+')
 
-print_line = 0
+
+for f in out_files:
+	if out_files.count(f) > 1:
+		print('\nError:\tinput-file and one output-file are the same, choose another output-file\n')
+		sys.exit()
+
+
+print('Input: ' + sys.argv[1] + '\tOutput: ' + '\t'.join(sys.argv[2:len(sys.argv)]))
 
 t_start = time.time()
 
 print('Converting fastPHASE output to LDhelmet input...')
 
 
-haplotype = 1
-	
-for line in fastphase_file:
-	if not line.startswith('END GENOTYPES'):
-		if line.startswith('# ID'):
-			sample_id = re.findall('#\sID\s(\d+)', line)
-			out_file.write('>sample_' + sample_id[0] + '_' + str(haplotype) + '\n')
-			print_line = 1
-		elif print_line == 1:
-			if haplotype == 1:
-				haplotype = 2
-			else:
-				out_file.write('>sample_' + sample_id[0] + '_' + str(haplotype) + '\n')
-				haplotype = 1
-			bases = line.strip('\n').split(' ')
-			sequence = ''.join(bases)
-			out_file.write(sequence + '\n')
 
-fastphase_file.close()
-out_file.close()
+haplotype = 0
+sample_id = 'None'
+sub_lab = 0
+
+with open(sys.argv[1], 'r') as fastphase_file:
+	for line in fastphase_file:
+
+		line_split = line.strip('\n').split(' ')
+
+		if line.startswith('# ID'):
+			haplotype = 1
+			sample_id = line_split[2]
+
+			if len(line_split) > 3:
+				sub_lab = int(line_split[7]) - 1
+
+			write_id(sample_id + '_' + str(haplotype), out_files[sub_lab])
+
+		elif haplotype == 1:
+			write_seq(line_split, out_files[sub_lab])
+			haplotype = 2
+
+		elif haplotype == 2:
+			write_id(sample_id + '_' + str(haplotype), out_files[sub_lab])
+			write_seq(line_split, out_files[sub_lab])
+			haplotype = 0
+
 
 elapsed = time.time() - t_start
 
 print('Done! Ran in: %.2fs' % elapsed)
 
+for f in out_files:
+	f.close()
