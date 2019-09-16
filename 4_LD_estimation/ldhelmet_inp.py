43b916e65c907b363fdd885eecd60ce68d9deeb6
Bella Sinclair <bsm.sinclair@gmail.com>
2019-09-16 15:38:44 +0200

    makes LDhelmet input and can take a file with a subset of samples

--- 4_LD_estimation/ldhelmet_inp.py
+++ 4_LD_estimation/ldhelmet_inp.py
@@ -12,35 +12,42 @@ def write_seq(line, file):
 
 
 
-if not len(sys.argv)>=3:
+if not len(sys.argv)==3 and not len(sys.argv)==4:
         print('\nError:\tincorrect number of command-line arguments')
-        print('Syntax:\tldhelmet_inp.py [FastPHASE Output] [Output File subpop. 1] optional: [[Output File subpop. 2] [Output File subpop. 3] ...\n')
+        print('Syntax:\tldhelmet_inp.py [FastPHASE Output] [Output File] optional: [Subset of samples]\n')
         sys.exit()
 
-
-out_files = [None]*(len(sys.argv)-2)
-
-for i in range(2,len(sys.argv)):
-	out_files[i-2] = open(sys.argv[i], 'w+')
-
-
-for f in out_files:
-	if out_files.count(f) > 1:
-		print('\nError:\tinput-file and one output-file are the same, choose another output-file\n')
+if sys.argv[1] == sys.argv[2]:
+		print('\nError:\tinput-file and output-file are the same, choose another output-file\n')
 		sys.exit()
 
 
-print('Input: ' + sys.argv[1] + '\tOutput: ' + '\t'.join(sys.argv[2:len(sys.argv)]))
+
+print('Input: ' + sys.argv[1] + '\tOutput: ' + sys.argv[2])
+
+
+subset_samples = []
+
+if len(sys.argv) == 4:
+	print('Output contain only subset of samples:')
+
+	with open(sys.argv[3], 'r') as sample_file:
+		for line in sample_file:
+			line = line.strip('\n')
+			subset_samples.append(line)
+
+	print(','.join(subset_samples))
+
+
 
 t_start = time.time()
 
 print('Converting fastPHASE output to LDhelmet input...')
 
 
-
+out_file = open(sys.argv[2], 'w+')
 haplotype = 0
 sample_id = 'None'
-sub_lab = 0
 
 with open(sys.argv[1], 'r') as fastphase_file:
 	for line in fastphase_file:
@@ -48,21 +55,20 @@ with open(sys.argv[1], 'r') as fastphase_file:
 		line_split = line.strip('\n').split(' ')
 
 		if line.startswith('# ID'):
-			haplotype = 1
 			sample_id = line_split[2]
-
-			if len(line_split) > 3:
-				sub_lab = int(line_split[7]) - 1
-
-			write_id(sample_id + '_' + str(haplotype), out_files[sub_lab])
+			if len(subset_samples) == 0 or sample_id in subset_samples:
+				haplotype = 1
+				write_id(sample_id + '_' + str(haplotype), out_file)
+			else:
+				haplotype = 0
 
 		elif haplotype == 1:
-			write_seq(line_split, out_files[sub_lab])
+			write_seq(line_split, out_file)
 			haplotype = 2
 
 		elif haplotype == 2:
-			write_id(sample_id + '_' + str(haplotype), out_files[sub_lab])
-			write_seq(line_split, out_files[sub_lab])
+			write_id(sample_id + '_' + str(haplotype), out_file)
+			write_seq(line_split, out_file)
 			haplotype = 0
 
 
@@ -70,5 +76,4 @@ elapsed = time.time() - t_start
 
 print('Done! Ran in: %.2fs' % elapsed)
 
-for f in out_files:
-	f.close()
+out_file.close()
