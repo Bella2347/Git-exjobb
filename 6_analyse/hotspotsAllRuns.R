3fd3ec76103685f6118821d32c0ff84b8cafb16e
bella <bsm.sinclair@gmail.com>
2019-09-26 11:18:05 +0200

    can read in any number of input files, assumes one file/run

--- 6_analyse/hotspotsAllRuns.R
+++ 6_analyse/hotspotsAllRuns.R
@@ -1,48 +1,72 @@
-# Put all hotspots in the same file...
+####### About ###############
+# Takes as input:
+# [out file] [hotspots in file1] [file2] ...
+# Writes the hotspots which are present in all infiles to the out file
+# Works for any number of infiles
 
 
-minHotspotLen <- 750
+######### FUNCTIONS ###################
 
 overlap <- function(listOfHotspots) {
+  minHotspotLen <- 750
   
-  if (min(listOfHotspots[,2]) - max(listOfHotspots[,1]) >= minHotspotLen) {
+  if (min(listOfHotspots[,3]) - max(listOfHotspots[,2]) >= minHotspotLen) {
     return(TRUE)
   } else {
     return(FALSE)
   }
 }
 
-argv <- commandArgs(trailingOnly = TRUE)
+get_hotspots <- function(hotspotsList, nrFiles) {
+  
+  hotspots <- as.data.frame(matrix(NA, dim(hotspotsList)[1],3))
+  colnames(hotspots) <- c("Start_SNP", "End_SNP", "Hotspot_length")
+  
+  for (i in 1:(dim(hotspotsList)[1] - nrFiles + 1)) {
+    
+    if (overlap(hotspotsList[i:(i + nrFiles - 1),])) {
+      hotspots[i,] <- c(max(hotspotsList[i:(i + nrFiles - 1),2]), min(hotspotsList[i:(i + nrFiles - 1),3]), 
+                        min(hotspotsList[i:(i + nrFiles - 1),3]) - max(hotspotsList[i:(i + nrFiles - 1),2]))
+    }
+  }
+  
+  hotspots <- na.omit(hotspots)
+  return(hotspots)
+}
 
-run1filename <- argv[1]
-run2filename <- argv[2]
-run3filename <- argv[3]
-run4filename <- argv[4]
-run5filename <- argv[5]
-outFilename <- argv[6]
-
-hrun1 <- read.table(run1filename, sep = "\t", header = TRUE)
-hrun2 <- read.table(run2filename, sep = "\t", header = TRUE)
-hrun3 <- read.table(run3filename, sep = "\t", header = TRUE)
-hrun4 <- read.table(run4filename, sep = "\t", header = TRUE)
-hrun5 <- read.table(run5filename, sep = "\t", header = TRUE)
-
-hruns <- rbind(hrun1, hrun2, hrun3, hrun4, hrun5)
-hruns <- hruns[order(hruns$Start.position),]
-
-
-hotspots <- as.data.frame(matrix(NA, dim(hrun1)[1],3))
-colnames(hotspots) <- colnames(hruns)
-h_i <- 1
-
-for (i in 1:(dim(hruns)[1]-4)) {
-
-  if (overlap(hruns[i:(i+4),])) {
-    hotspots[h_i,] <- c(max(hruns[i:(i+4),1]), min(hruns[i:(i+4),2]), min(hruns[i:(i+4),2]) - max(hruns[i:(i+4),1]))
-    h_i <- h_i + 1
+main <- function(argv) {
+  argv <- commandArgs(trailingOnly = TRUE)
+  
+  outFilename <- argv[1]
+  write("#Start_pos\tEnd_post\tLength_of_hotspot", outFilename)
+  
+  # Read in the files
+  hotspotsRuns <- lapply(argv[2:length(argv)], read.table, sep = "\t", header = FALSE)
+  
+  # rbind the datframes to one
+  hotspotsAll <- do.call(rbind, hotspotsRuns)
+  colnames(hotspotsAll) <- c("Scaffold", "Start_SNP", "End_SNP", "Hotspot_length")
+  
+  # sort on scaffoldname and start position
+  hotspotsAllSorted <- hotspotsAll[order(hotspotsAll$Scaffold, hotspotsAll$Start_SNP),]
+  
+  # Count how many infiles there are
+  nrInputFiles <- length(argv) - 1
+  
+  # if five enteries next to each other overlap, return as hotspot if the overlap is at least 750
+  hotspots <- get_hotspots(hotspotsAllSorted, nrInputFiles)
+  
+  # if we got any hotspots append to file with: scaffoldname, start, end, length
+  if (length(unlist(hotspots)) > 0) {
+    write.table(hotspots, outFilename, append = TRUE, sep = "\t", row.names = FALSE, col.names = FALSE)
   }
 }
 
-hotspots <- hotspots[1:(h_i-1),]
 
-write.table(hotspots, outFilename, append = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+########## Execution ######################
+
+main()
+
+
+
+
