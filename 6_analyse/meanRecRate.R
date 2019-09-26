0d19599c11f0333bb8c306baa58efc0b10237364
bella <bsm.sinclair@gmail.com>
2019-09-26 13:51:55 +0200

    uses main function, appends to out file

--- 6_analyse/meanRecRate.R
+++ 6_analyse/meanRecRate.R
@@ -9,34 +9,38 @@ index_from_one <- function(recombinationRate) {
   return(recombinationRateNewPos)
 }
 
+main <- function(fileList) {
+  outfile <- fileList[1]
+  
+  # Read in the input files
+  infiles <- lapply(fileList[2:6], read.table, sep = " ", skip = 2, header = FALSE)
+  
+  # The file is indexed from 0, add one to each position to change the indexing to 1
+  infilesIndexOne <- lapply(infiles, index_from_one)
+  
+  # Bind all columns
+  allRuns <- do.call(cbind, infilesIndexOne)
+  
+  # Save the positions
+  recRate <- allRuns[,1:2]
+  
+  # Creat data frame with only the rec rate
+  allRunsRecRate <- allRuns[,c(3,6,9,12,15)]
+  
+  # Calculate the mean rec rate for each SNP pair
+  meanRecRate <- apply(allRunsRecRate, 1, mean)
+  
+  # Bind the mean rec rate to the positions
+  recRate <- cbind(recRate, meanRecRate)
+  colnames(recRate) <- c("Start SNP", "End SNP", "Mean rec. rate over all runs")
+  
+  # Write mean rec. rate to file, no column names since we append
+  write.table(recRate, outfile, append = TRUE, sep = "\t", row.names = FALSE, col.names = FALSE)
+  
+}
+
 #####
 
 argv <- commandArgs(trailingOnly = TRUE)
 
-outfile <- argv[1]
-
-infiles <- lapply(argv[2:6], read.table, sep = " ", skip = 2, header = FALSE)
-
-##########
-
-# The file is indexed from 0, add one to each position to change the indexing to 1
-infilesIndexOne <- lapply(infiles, index_from_one)
-
-# Bind all columns
-allRuns <- do.call(cbind, infilesIndexOne)
-
-# Save the positions
-recRate <- allRuns[,1:2]
-
-# Creat data frame with only the rec rate
-allRunsRecRate <- allRuns[,c(3,6,9,12,15)]
-
-# Calculate the mean rec rate for each SNP pair
-meanRecRate <- apply(allRunsRecRate, 1, mean)
-
-# Bind the mean rec rate to the positions
-recRate <- cbind(recRate, meanRecRate)
-colnames(recRate) <- c("Start SNP", "End SNP", "Mean rec. rate over all runs")
-
-# Write mean rec. rate to file, no column names since we append
-write.table(recRate, outfile, append = TRUE, sep = "\t", row.names = FALSE, col.names = FALSE)
+main(argv)
