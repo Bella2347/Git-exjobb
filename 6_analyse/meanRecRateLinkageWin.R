8b7101475598e7e770d0b0f74d4cf52f23453d8a
bella <bsm.sinclair@gmail.com>
2019-10-04 14:13:31 +0200

    uses another approach, creats array instead of sum, outputs variance

--- 6_analyse/meanRecRateLinkageWin.R
+++ 6_analyse/meanRecRateLinkageWin.R
@@ -4,67 +4,60 @@
 # Commandline argumenst:
 # [recombination rate] [linkage map windows] [out file]
 
-sum_rec_rate <- function(snp, startIndex, endIndex) {
-  # Returns the sum of the recombination rate at bases for a pair of SNPs
-  # which are inside the window, it the SNP pair wraps the ends,
-  # only the bases until the window end is sumed
+recombination_rate_each_base <- function(recombinationRate, win) {
+  # Returns an array were each value represents one base and the recombination rate at that base
   
-  # Add 1 to the end so the end is included
-  endIndex <- endIndex + 1
+  windowLength <- win[2] - win[1] + 1
   
-  # If there is no overlap, return 0
-  if (as.numeric(snp[1]) >= endIndex || as.numeric(snp[2]) <= startIndex) {
-    return(0)
+  recombinationRate[,2] <- as.numeric(recombinationRate[,2]) - win[1] + 1
+  recombinationRate[,3] <- as.numeric(recombinationRate[,3]) - win[1] + 1
+  
+  if (as.numeric(recombinationRate[dim(recombinationRate)[1],3]) > windowLength) {
+    recombinationRate[dim(recombinationRate)[1],3] <- windowLength + 1
   }
   
-  # If start SNP is after the window start, use the SNP position
-  if (as.numeric(snp[1]) >= startIndex) {
-    startIndex <- as.numeric(snp[1])
+  if (as.numeric(recombinationRate[1,2]) < 1) {
+    recombinationRate[1,2] <- 1
   }
   
-  # If the last SNP is before the end, use that SNP position
-  if (as.numeric(snp[2]) < endIndex) {
-    endIndex <- as.numeric(snp[2])
+  # An array that is as long as the window
+  recombinationRateArray <- array(NA, windowLength)
+  
+  # For each base write the recombination rate
+  for (i in 1:dim(recombinationRate)[1]) {
+    recombinationRateArray[as.numeric(recombinationRate[i,2]):(as.numeric(recombinationRate[i,3])-1)] <- recombinationRate[i,4]
   }
   
-  # Take the rec rate times the number of bases between the SNPs that are in the window
-  sumrate <- (endIndex - startIndex)*as.numeric(snp[3])
-  
-  return(sumrate)
+  return(recombinationRateArray)
 }
 
 
 mean_in_win <- function(linkageWin, recombinationRate) {
   # Takes a window and computes the mean recombination rate in that window
   
-  # Takes all SNP pairs in a certain scaffold
-  recRateScaffold <- recombinationRate[recombinationRate[,1] == linkageWin[2], ]
+  # Takes all SNP pairs in the linkage win
+  snpsWin <- recombinationRate[recombinationRate[,1] == linkageWin[2] &  
+                                 as.numeric(recombinationRate[,2]) <= as.numeric(linkageWin[4]) &
+                                 as.numeric(recombinationRate[,3]) > as.numeric(linkageWin[3]), ]
   
-  # If no SNP pairs are returned NA is returned
-  if (dim(recRateScaffold)[1] != 0) {
-    # For each SNP pair, get the sum over bases that are in the window
-    recRateSum <- sum(apply(recRateScaffold[,2:4], 1, sum_rec_rate, startIndex = as.integer(linkageWin[3]), 
-                            endIndex = as.integer(linkageWin[4])))
-
-    # Get the end SNP
-    endSNP <- as.numeric(max(recRateScaffold[as.numeric(recRateScaffold[,2]) <= as.numeric(linkageWin[4]),][,3]))
-
-    startSNP <- as.numeric(min(recRateScaffold[as.numeric(recRateScaffold[,3]) > as.numeric(linkageWin[3]),][,2]))
-
-    if (endSNP > as.numeric(linkageWin[4])) {
-      endSNP <- as.numeric(linkageWin[4]) + 1
-    }
-
-    if (startSNP < as.numeric(linkageWin[3])) {
-      startSNP <- as.numeric(linkageWin[3])
-    }
-
-    meanRecRate <- recRateSum/(endSNP - startSNP)
+  
+  
+  # If no SNP pairs are returned, or there are no SNPs in the window
+  # NA is returned
+  if (dim(snpsWin)[1] > 1) {
     
-    endSNP <- endSNP - 1
+    recRateVar <- var(as.numeric(snpsWin[,4]))
+    
+    baseArray <- recombination_rate_each_base(snpsWin, c(as.numeric(linkageWin[3]), as.numeric(linkageWin[4])))
 
-    meanRecRateWin <- c(linkageWin[1:4], as.character(startSNP), as.character(endSNP), meanRecRate)
-
+    meanRecRate <- mean(as.numeric(baseArray), na.rm = TRUE)
+    
+    meanRecRateWin <- c(linkageWin[1:4], dim(snpsWin)[1], meanRecRate, recRateVar)
+  
+  } else if (dim(snpsWin)[1] == 1) {
+    
+    meanRecRateWin <- c(linkageWin[1:4], dim(snpsWin)[1], snpsWin[4], "NA")
+    
   } else {
 
     meanRecRateWin <- c(linkageWin[1:4], "NA", "NA", "NA")
@@ -90,12 +83,11 @@ main <- function(fileList) {
   
   # Transform to a data.frame and add column names
   meanRecRateWin <- as.data.frame(t(meanRecRateWin))
-  colnames(meanRecRateWin) <- c("Chr", "Scaffold", "LinkWin_start", "LinkWin_end", "RecWin_start", "RecWin_end", "Mean_recRate")
+  colnames(meanRecRateWin) <- c("Chr", "Scaffold", "LinkWin_start", "LinkWin_end", "#_SNPs_in_window", 
+                                "Mean_recRate", "Variance")
   
   # Sort the results on scaffold
   meanRecRateWin <- meanRecRateWin[order(meanRecRateWin$Scaffold, meanRecRateWin$LinkWin_start),]
-  
-  #options(scipen = 999)
   
   write.table(meanRecRateWin, outfile, append = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
 }
